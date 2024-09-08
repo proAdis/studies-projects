7bc51423b6e16f0a3b9c709518a849434e131b92
proAdis <145547050+proAdis@users.noreply.github.com>
2024-09-08 05:58:30 +0300

    HTML, JavaScript and CSS Project

---
+++ to_do_list/ToDoList.js
@@ -0,0 +1,45 @@
+const todoList = [
+];
+
+renderTodoList();
+
+function renderTodoList(){
+  let todoListHTML='';
+
+  todoList.forEach(function(todoObject,index ){
+   // const name=todoObject.name;
+   // const dueDate=todoObject.dueDate;
+   const {name,dueDate}=todoObject;
+    const html=`
+    <div>${name}</div>
+     <div>${dueDate}</div>
+     <button onclick="
+     todoList.splice(${index},1);
+     renderTodoList();
+     " class="delete-button">delete</button>
+    `;
+    todoListHTML+=html;
+  });
+
+  document.querySelector('.js-todo-list')
+  .innerHTML=todoListHTML;
+  }
+
+
+function addTodo()
+{
+  const inputElement = document.querySelector('.js-name-input');
+ const name= inputElement.value;
+   
+const dateInputElement=document.querySelector('.js-date-input');
+const dueDate=dateInputElement.value;
+
+ todoList.push({
+  //name: name,
+  //dueDate:dueDate
+  name,dueDate
+});
+
+ inputElement.value=''; 
+ renderTodoList();
+}
